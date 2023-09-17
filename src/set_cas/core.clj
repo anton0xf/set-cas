52d67fb4843a5da031efb2360f617910a23d8e56
anton0xf <anton0xf@gmail.com>
2023-09-17 13:07:24 +0300

    implement match() and find-subexpr()

--- src/set_cas/core.clj
+++ src/set_cas/core.clj
@@ -33,6 +33,8 @@
 
         :else expr))
 
+;; TODO write replace-one
+;; TODO rename to replace-all and rename vars to m (replacement map)
 (defn replace-vars [expr vars]
   (cond (coll? expr)
         (let [[op & args] expr
@@ -40,6 +42,52 @@
           (cons op args))
 
         :else (vars expr expr)))
+
+(defn match
+  "Try to match an expression [expr] with a pattern [p] with free variables [vars].
+  Return map of variables values or [nil] if the pattern doesn't match.
+  Already assigned values passed as [vs] map"
+  ([expr p vars] (match expr p vars {}))
+  ([expr p vars vs]
+   (cond (and (symbol? p) (vars p))
+         (if-let [val (vs p)]
+           (if (= expr val) vs nil)
+           (assoc vs p expr))
+
+         (and (coll? p) (coll? expr)
+              (= (first p) (first expr)))
+         (loop [exprs (rest expr)
+                ps (rest p)
+                vs vs]
+           (cond (and (empty? ps) (empty? exprs)) vs
+                 (or (empty? ps) (empty? exprs)) nil
+                 :else (when-let [vs (match (first exprs) (first ps) vars vs)]
+                         (recur (rest exprs) (rest ps) vs)))))))
+
+(defn subexpr-dfs
+  "Return sequence of pairs [path expression] for all subexpressions of [expr]"
+  ([expr] (subexpr-dfs expr []))
+  ([expr path-prefix]
+   (lazy-seq
+    (cons [path-prefix expr]
+          (if (coll? expr)
+            (reduce
+             concat '()
+             (map-indexed
+              (fn [idx val]
+                (subexpr-dfs val (conj path-prefix (inc idx))))
+              (rest expr))))))))
+
+(defn find-subexpr
+  "Find the first match of pattern [p]
+  through all subexpressions (dfs) of [expr].
+  Return map with :path in original expression and matched values :vals"
+  [expr p vars]
+  (->> (subexpr-dfs expr)
+       (map (fn [[path expr]]
+              (if-let [vs (match expr p vars)]
+                {:path path :vals vs})))
+       (some identity)))
 
 (defn subst' [expr lhs rhs]
   (cond (= lhs expr) rhs
--- test/set_cas/core_test.clj
+++ test/set_cas/core_test.clj
@@ -18,8 +18,63 @@
   (is (= 'A (replace-vars 'X {'X 'A})))
   (is (= 'Y (replace-vars 'Y {'X 'A})))
   (is (= '(+ A Y) (replace-vars '(+ X Y) {'X 'A})))
+  (is (= '(+ (- A B) Y) (replace-vars '(+ X Y) {'X '(- A B)})))
   (is (= '(+ A B) (replace-vars '(+ X Y) {'X 'A, 'Y 'B})))
   (is (= '(+ A (- A B)) (replace-vars '(+ A (- X B)) {'X 'A}))))
+
+(deftest test-match
+  (is (= {'X 'A} (match 'A 'X #{'X})))
+  (is (= {'X 'A, 'Y 0}
+         (match 'A 'X #{'X 'Y} {'Y 0})))
+  (is (= {'X 0} (match 0 'X #{'X})))
+  (is (= {'X 'A} (match 'A 'X #{'X 'Y})))
+  (is (= {'X '(+ A B)} (match '(+ A B) 'X #{'X})))
+  (is (= {'X 'A} (match 'A 'X #{'X} {'X 'A})))
+  (is (nil? (match 'A 'X #{'X} {'X 'B})))
+  (is (= {} (match '(+) '(+) #{})))
+  (is (nil? (match '(+) '(-) #{})))
+  (is (= {} (match '(+) '(+) #{'X})))
+  (is (nil? (match '(+ A) '(+ A B) #{})))
+  (is (nil? (match '(+ A) '(+ B) #{'X})))
+  (is (= {'X 'A} (match '(+ A) '(+ X) #{'X})))
+  (is (nil? (match '(+ A) '(+ X) #{'X} {'X 'B})))
+  (is (nil? (match '(+ A B) '(+ X X) #{'X})))
+  (is (= {'X 'A} (match '(+ A A) '(+ X X) #{'X})))
+  (is (= {'X 'A, 'Y 'B} (match '(+ A B) '(+ X Y) #{'X 'Y})))
+  (is (= {'X 'A, 'Y 'A} (match '(+ A A) '(+ X Y) #{'X 'Y})))
+  (is (= {'X 'A, 'Y 'B} (match '(+ A B A) '(+ X Y X) #{'X 'Y})))
+  (is (nil? (match '(+ A B B) '(+ X Y X) #{'X 'Y})))
+  (is (= {'X 'A, 'Y 'B} (match '(+ A (- B A)) '(+ X (- Y X)) #{'X 'Y})))
+  (is (nil? (match '(+ A (- B C)) '(+ X (- Y X)) #{'X 'Y}))))
+
+(deftest test-subexpr-dfs
+  (is (= '([[] A]) (subexpr-dfs 'A)))
+  (is (= '([[] (+ A)] [[1] A]) (subexpr-dfs '(+ A))))
+  (is (= '([[] (+ A B)] [[1] A] [[2] B])
+         (subexpr-dfs '(+ A B))))
+  (is (= '([[] (+ A B C)] [[1] A] [[2] B] [[3] C])
+         (subexpr-dfs '(+ A B C))))
+  (is (= '([[] (+ (* A B) (- C D))]
+           [[1] (* A B)] [[1 1] A] [[1 2] B]
+           [[2] (- C D)] [[2 1] C] [[2 2] D])
+         (subexpr-dfs '(+ (* A B) (- C D))))))
+
+(deftest test-find-subexpr
+  (is (= {:path [] :vals {'X 'A}}
+         (find-subexpr 'A 'X #{'X})))
+  (is (nil? (find-subexpr '(+ A B) '(+ X X) #{'X})))
+  (is (= {:path [] :vals {'X '(+ A B)}}
+         (find-subexpr '(+ A B) 'X #{'X})))
+  (is (= {:path [] :vals {'X 'A}}
+         (find-subexpr '(+ A A) '(+ X X) #{'X})))
+  (is (= {:path [] :vals {'X 'A, 'Y 'B}}
+         (find-subexpr '(+ A B) '(+ X Y) #{'X 'Y})))
+  (is (= {:path [1] :vals {'X 'A, 'Y 'B}}
+         (find-subexpr '(* (+ A (- B A)) C) '(+ X (- Y X)) #{'X 'Y})))
+  (is (= {:path [2] :vals {'X 'B, 'Y 'A}}
+         (find-subexpr '(+ A (- B A)) '(- X Y) #{'X 'Y})))
+  (is (= {:path [2 1] :vals {'X 'C, 'Y 'D}}
+         (find-subexpr '(+ A (* (- C D) B)) '(- X Y) #{'X 'Y}))))
 
 (deftest test-subst
   (is (= '(+ B A) (subst '(+ A B) union-comm {'X 'A, 'Y 'B})))
